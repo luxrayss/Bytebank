75411919d6767d625973c1b61ecd27f54dc13abc
Wesley Souza <74992013+luxrayss@users.noreply.github.com>
2022-05-04 11:21:54 -0300

    att

--- lib/main.dart
+++ lib/main.dart
@@ -25,14 +25,36 @@ class FormularioTransferencia extends StatelessWidget {
         appBar: AppBar(title: Text('Criando Transferência'),),
         body: Column(
           children: [
-            TextField(
-              style: TextStyle(
-                fontSize: 16.0
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 24.0
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Número da Conta',
+                  hintText: '0000',
+                ),
+                keyboardType: TextInputType.number,
               ),
-              decoration: InputDecoration(
-                labelText: 'Número da Conta',
-                hintText: '0000',
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 24.0
+                ),
+                decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: 'Valor',
+                  hintText: '0,00',
+                ),
+                keyboardType: TextInputType.number,
               ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Confirmar'),
             ),
           ],
         ));
