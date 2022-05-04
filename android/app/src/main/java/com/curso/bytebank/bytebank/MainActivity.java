0101b25dd4b01f290164e93f701594413a3e7c36
Wesley Souza <74992013+luxrayss@users.noreply.github.com>
2022-05-04 09:08:10 -0300

    Initial commit

---
+++ android/app/src/main/java/com/curso/bytebank/bytebank/MainActivity.java
@@ -0,0 +1,6 @@
+package com.curso.bytebank.bytebank;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+//Quando for usado o RaisedButton, substituir por ElevatedButton
+
+//Iniciando um MaterialApp
+void main() => runApp(ByteBankApp());
+
+class ByteBankApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        //corpo do app
+        body: FormularioTransferencia(),
+      ),
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Criando Transferência'),),
+        body: Column(
+          children: [
+            TextField(),
+            TextField(),
+            ElevatedButton(onPressed: onPressed)
+          ],
+        ));
+  }
+}
+
+class ListaTransferencias extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    //Card ficará dentro de uma coluna para poder haver vários cards
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
+        children: <Widget>[
+          //Chamando o método do valor e conta
+          ItemTransferencia(Transferencia(100.00, 1000)),
+          ItemTransferencia(Transferencia(200.00, 2000)),
+          ItemTransferencia(Transferencia(300.00, 3000)),
+          ItemTransferencia(Transferencia(400.00, 4000)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        //icone do botão
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget{
+
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  //construtor
+  Transferencia(this.valor, this.numeroConta);
+}
